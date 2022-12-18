058abca498e3adb6f0b22ede16f0615da7ff35b0
Mrityunjay1 <singhmrityunjay32@gmail.com>
2022-12-18 18:25:03 +0530

    chart uploaded

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -1,6 +1,8 @@
-import { data } from "autoprefixer";
 import React, { useEffect, useState } from "react";
+import { Pie } from "react-chartjs-2";
 import { useParams } from "react-router-dom";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+ChartJS.register(ArcElement, Tooltip, Legend);
 
 const Details = () => {
   const { id } = useParams();
@@ -30,7 +32,7 @@ const Details = () => {
               Device ID
             </th>
             <th scope="col" className="py-3 px-6">
-              Device Type
+              Location
             </th>
           </tr>
         </thead>
@@ -44,13 +46,45 @@ const Details = () => {
                 >
                   {item.device_id}
                 </th>
-                <td className="py-4 px-6">{item.device_type}</td>
+                <td className="py-4 px-6">{item.location}</td>
               </tr>
             );
           })}
         </tbody>
       </table>
-      <div>hello</div>
+      <div>
+        <Pie
+          width={400}
+          height={300}
+          data={{
+            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+            datasets: [
+              {
+                label: "# of Votes",
+                data: [12, 19],
+                backgroundColor: [
+                  "rgba(255, 99, 132, 0.2)",
+                  "rgba(54, 162, 235, 0.2)",
+                  "rgba(255, 206, 86, 0.2)",
+                  "rgba(75, 192, 192, 0.2)",
+                  "rgba(153, 102, 255, 0.2)",
+                  "rgba(255, 159, 64, 0.2)",
+                ],
+                borderColor: [
+                  "rgba(255, 99, 132, 1)",
+                  "rgba(54, 162, 235, 1)",
+                  "rgba(255, 206, 86, 1)",
+                  "rgba(75, 192, 192, 1)",
+                  "rgba(153, 102, 255, 1)",
+                  "rgba(255, 159, 64, 1)",
+                ],
+                borderWidth: 1,
+              },
+            ],
+          }}
+          options={{}}
+        />
+      </div>
     </div>
   );
 };
